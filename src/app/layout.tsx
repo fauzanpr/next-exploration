e3670a35590db5ebd7d0afcd42a7ba8d060315f2
Fauzan Pradana <fauzanpr.06@gmail.com>
2024-08-23 13:47:24 +0700

    feat(home): adding on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Button, Flex, Layout, Space } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import Title from 'antd/es/typography/Title';
+import Link from "next/link";
+import Text from "antd/es/typography/Text";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +21,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Layout style={{
+          minHeight: "100vh"
+        }}>
+          <Header style={{
+            height: "fit-content",
+            paddingBottom: "16px",
+            paddingTop: "16px"
+          }}>
+            <Flex justify='space-between' align='center'>
+              <Title level={2} type='warning' style={{
+                marginBottom: 0,
+              }}>📌 Promptopia</Title>
+              <Link href="/add">
+                <Button type="primary">Add Prompt</Button>
+              </Link>
+            </Flex>
+          </Header>
+          <Content style={{
+            padding: "48px",
+          }}>
+            { children }
+          </Content>
+          <Footer>
+            <Flex justify='center'>
+              <Text>Copyright @2024</Text>
+            </Flex>
+          </Footer>
+        </Layout>
+      </body>
     </html>
   );
 }
